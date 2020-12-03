d8516bb513d403b83721a887d0a89e08619db671
Guillem Vela <guillemglez@gmail.com>
2020-12-03 11:48:54 +0100

    Day 3, part 2

--- 3/trees.py
+++ 3/trees.py
@@ -1,12 +1,44 @@
+from typing import List
+
+slopes: List[dict] = [{
+    "x": 1,
+    "y": 1
+}, {
+    "x": 3,
+    "y": 1
+}, {
+    "x": 5,
+    "y": 1
+}, {
+    "x": 7,
+    "y": 1
+}, {
+    "x": 1,
+    "y": 2
+}]
+
+for slope in slopes:
+    slope["trees"] = 0
+    slope["pos"] = 0
+
 with open("input") as f:
-    x = 0
-    trees = 0
-    for line in f:
-        char = x % (len(line) - 1)  # ignore \n
+    for i, line in enumerate(f):
+        for slope in slopes:
+            if (i % slope["y"]):
+                continue
 
-        if line[char] == "#":
-            trees += 1
+            char = slope["pos"] % (len(line) - 1)  # ignore \n
 
-        x += 3
+            if line[char] == "#":
+                slope["trees"] += 1
 
-print(f"You have encountered {trees} trees")
+            slope["pos"] += slope["x"]
+
+product = 1
+for slope in slopes:
+    if slope["x"] == 3:
+        trees = slope["trees"]
+    product *= slope["trees"]
+
+print(f"You have encountered {trees} trees following slope (3, 1)")
+print(f"Second part result is {product}")
