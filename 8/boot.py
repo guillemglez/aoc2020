f719de2b98b246fd8a93382074288832a5bf7077
guillemglez <guillemglez@gmail.com>
2020-12-08 08:31:35 +0100

    Day 8, part 2

--- 8/boot.py
+++ 8/boot.py
@@ -4,22 +4,47 @@ with open('input') as f:
     inpt = f.read()
 
 code: List[Tuple[str, int]] = []
-for instruction in inpt.split('\n'):
-    operation = instruction.split()[0]
-    argument = int(instruction.split()[1])
+for rawinstruction in inpt.split('\n'):
+    operation = rawinstruction.split()[0]
+    argument = int(rawinstruction.split()[1])
     code.append((operation, argument))
 
-ran: Dict[int, bool] = {i: False for i in range(len(code))}
-accumulator: int = 0
-line: int = 0
-while not ran[line]:
-    ran[line] = True
-    op, arg = code[line]
+def run(code: List[Tuple[str, int]]) -> int:
+    ran: Dict[int, bool] = {i: False for i in range(len(code))}
+    accumulator: int = 0
+    line: int = 0
+    while line < len(code) and not ran[line]:
+        ran[line] = True
+        op, arg = code[line]
+        if op == 'acc':
+            accumulator += arg
+        if op == 'jmp':
+            line += arg - 1
+        line += 1
+    return accumulator
+
+def ishealthy(code: List[Tuple[str, int]]) -> bool:
+    ran: Dict[int, bool] = {i: False for i in range(len(code))}
+    line: int = 0
+    while line < len(code):
+        if ran[line]:
+            return False
+        else:
+            ran[line] = True
+        op, arg = code[line]
+        if op == 'jmp':
+            line += arg - 1
+        line += 1
+    return line == len(code)
+
+for line, instruction in enumerate(code):
+    op, arg = instruction
     if op == 'acc':
-        accumulator += arg
-    if op == 'jmp':
-        line += arg
         continue
-    line += 1
+    fixed = code.copy()
+    fixed[line] = ('jmp' if op == 'nop' else 'nop', arg)
+    if ishealthy(fixed):
+        break
     
-print(f"The accumulator value is {accumulator}")
+print(f"When broken, it crashes with the accumulator value being {run(code)}.")
+print(f"Once fixed, it exits with the accumulator value being {run(fixed)}.")
