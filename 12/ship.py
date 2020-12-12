158a8e572da0dd504699e6dceb2df9e05d67c7a5
guillemglez <guillemglez@gmail.com>
2020-12-12 11:21:30 +0100

    Day 12, part 2

--- 12/ship.py
+++ 12/ship.py
@@ -1,5 +1,6 @@
 import math
 
+## PART 1
 with open('input') as f:
     ship = {'x': 0., 'y': 0., 'd': 0}
     for line in f:
@@ -23,4 +24,38 @@ with open('input') as f:
             ship['x'] += math.cos(math.radians(ship['d'])) * argument
             ship['y'] += math.sin(math.radians(ship['d'])) * argument
 
-print(f"The Manhattan distance is {int(abs(ship['x']) + abs(ship['y']))}")
+print(f"The Manhattan distance is {round(abs(ship['x']) + abs(ship['y']))}")
+
+## PART 2
+with open('input') as f:
+    ship = {'x': 0., 'y': 0., 'dx': 10, 'dy': 1}
+    for line in f:
+        instruction = line[0]
+        argument = int(line.strip()[1:])
+
+        if instruction == 'E':
+            ship['dx'] += argument
+        if instruction == 'N':
+            ship['dy'] += argument
+        if instruction == 'W':
+            ship['dx'] -= argument
+        if instruction == 'S':
+            ship['dy'] -= argument
+
+        if instruction == 'F':
+            ship['x'] += ship['dx'] * argument
+            ship['y'] += ship['dy'] * argument
+
+        # https://en.wikipedia.org/wiki/Rotation_matrix#In_two_dimensions
+        if instruction == 'L' or instruction == 'R':
+            if instruction == 'R':
+                argument *= -1
+            x = ship['dx']
+            ship['dx'] = x * math.cos(math.radians(
+                argument)) - ship['dy'] * math.sin(math.radians(argument))
+            ship['dy'] = x * math.sin(math.radians(
+                argument)) + ship['dy'] * math.cos(math.radians(argument))
+
+print(
+    f"The corrected Manhattan distance is {round(abs(ship['x']) + abs(ship['y']))}"
+)
