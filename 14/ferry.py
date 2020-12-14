65e9d86b95ba2a6bfc958852fd69b70ed866f6e2
guillemglez <guillemglez@gmail.com>
2020-12-14 22:50:49 +0100

    Day 14, part 2

--- 14/ferry.py
+++ 14/ferry.py
@@ -1,4 +1,4 @@
-from typing import Tuple, Dict
+from typing import Tuple, Dict, List
 
 
 def readmask(mask: str) -> Tuple[int, int]:
@@ -30,4 +30,48 @@ with open('input') as f:
 
 print(
     f"The sum of all values left in memory after it completes is {sum(mem.values())}"
+)
+
+
+def readfmask(mask: str) -> int:
+    fmask = 0
+    for i, char in enumerate(mask):
+        if char == 'X':
+            fmask += 1 << (35 - i)
+    return fmask
+
+
+def applyfmask(faddress: int, fmask: int) -> List[int]:
+    indexes: List[int] = []
+    for b in range(36):
+        if fmask & 1 << b:
+            indexes.append(b)
+    addresses: List[int] = []
+    for i in range(1 << len(indexes)):
+        address = faddress
+        for b in range(len(indexes)):
+            if i & 1 << b:
+                address |= 1 << indexes[b]
+            else:
+                address &= ~(1 << indexes[b])
+        addresses.append(address)
+    return addresses
+
+
+mem.clear()
+with open('input') as f:
+    for line in f:
+        if 'mask' in line:
+            # Discard lowmask, since now 0s become unchanged
+            mask = 0, readmask(line.split('=')[1].strip())[1]
+            fmask = readfmask(line.split('=')[1].strip())
+        if 'mem' in line:
+            rawaddress = int(line[line.find('[') + 1:line.find(']')])
+            faddress = applymask(rawaddress, mask)
+            value = int(line.split('=')[1].strip())
+            for address in applyfmask(faddress, fmask):
+                mem[address] = value
+
+print(
+    f"Following the second version, the sum of all values is {sum(mem.values())}"
 )
