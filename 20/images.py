8510b393d3dea070666e16bdc4f77cfd29fa63bd
guillemglez <guillemglez@gmail.com>
2020-12-21 12:40:18 +0100

    Day 20, part 1 - properly done

--- 20/images.py
+++ 20/images.py
@@ -1,5 +1,8 @@
 from typing import List, Tuple, Dict
 import numpy as np
+import math
+
+from tile import Tile
 
 BLACK = '#'
 WHITE = '.'
@@ -7,42 +10,59 @@ WHITE = '.'
 with open('input') as f:
     inpt = f.read().strip()
 
-tiles: Dict[int, np.ndarray] = {}
+# Parse input to get the tiles, store them in the "bag"
+# Store all the borders in a list to check them later
+bag: Dict[int, Tile] = {}
+borders: List[int] = []
 for rawtile in inpt.split('\n\n'):
     rows = rawtile.split('\n')
     tileid = int(rows.pop(0).split()[-1][:-1])  # "Tile XXXX:"
     tile = np.where(np.array([list(r) for r in rows]) == WHITE, 1, 0)
-    tiles[tileid] = tile
+    bag[tileid] = Tile(tileid, tile)
+    for b in range(4):
+        borders.append(bag[tileid].getborder(b))
+        borders.append(bag[tileid].getborder(b, True))
 
+# Look for a corner tile: only two borders of it match other pieces'
+for cornertile in bag.values():
+    invalid = []
+    for b in range(4):
+        invalid.append(borders.count(cornertile.getborder(b, True)) == 1)
+    if invalid.count(True) == 2:
+        break
 
-# Convert border to an int ID (bit representation)
-def bordertoint(pixels: np.ndarray) -> int:
-    border = 0
-    for i in range(len(pixels)):
-        if pixels[i] > 0:
-            border += (1 << i)
-    return border
+# Correctly orientate the corner tile
+while not invalid == [True, False, False, True]:
+    cornertile.rotate()
+    invalid = []
+    for b in range(4):
+        invalid.append(borders.count(cornertile.getborder(b, True)) == 1)
 
+# Create a square canvas and place the corner tile
+canvas: np.ndarray = np.zeros((int(math.sqrt(len(bag))), ) * 2, dtype=object)
+canvas[0, 0] = bag.pop(cornertile.id)
 
-borders: Dict[int, List[int]] = {}
-for tileid, tile in tiles.items():
-    borders[tileid] = [
-        bordertoint(tile[0, ...].flatten()),
-        bordertoint(tile[..., -1].flatten()),
-        bordertoint(tile[-1, ::-1].flatten()),
-        bordertoint(tile[::-1, 0].flatten())
-    ]
+# Place the tiles where they fit in the canvas
+while len(bag):
+    # Check each tile still in the bag
+    for tile in bag.copy().values():
+        # Flip it in both axis
+        for flip in range(2):
+            # Rotate it 90, 180 and 270 degrees clockwise
+            for _ in range(3):
+                for idx in np.ndindex(canvas.shape):
+                    if canvas[idx] == 0 and tile.fitsin(canvas, *idx):
+                        canvas[idx] = bag.pop(tile.id)
+                        break
+                if tile.id not in bag.keys():
+                    break
+                else:
+                    tile.rotate()
+            if tile.id not in bag.keys():
+                break
+            else:
+                tile.flip(flip)
 
-corners = 1
-allborders = np.array([v for v in borders.values()]).flatten()
-for tileid, bordercodes in borders.items():
-    # Whether there is another equal border (the same border, but reversed, should also be looked into)
-    other = [(np.count_nonzero(allborders == bordercode) + np.count_nonzero(
-        allborders == int('{:010b}'.format(bordercode)[::-1], 2))) > 1
-             for bordercode in bordercodes]
-    # Find whether it is a corner
-    if other in [[True, True, False, False], [False, True, True, False],
-                 [False, False, True, True], [True, False, False, True]]:
-        corners *= tileid
-
-print(f"The four corner tiles multiplied give {corners}")
+print(
+    f"The four corner tiles multiplied give {canvas[0,0].id * canvas[0,-1].id * canvas[-1,0].id * canvas[-1,-1].id}"
+)
---
+++ 20/tile.py
@@ -0,0 +1,119 @@
+import numpy as np
+
+
+class Tile:
+    TOP = 0
+    RIGHT = 1
+    BOTTOM = 2
+    LEFT = 3
+
+    VERTICAL = 0
+    HORIZONTAL = 1
+
+    def __init__(self, tileid: int, image: np.ndarray):
+        self.id = tileid
+        # TOP->0
+        # RIGHT->1
+        # BOTTOM->2
+        # LEFT->3
+        self.borders = [
+            self.bordertoint(image[0, :].flatten()),
+            self.bordertoint(image[:, -1].flatten()),
+            self.bordertoint(image[-1, ::-1].flatten()),
+            self.bordertoint(image[::-1, 0].flatten())
+        ]
+
+    def bordertoint(self, pixels: np.ndarray) -> int:
+        """Convert border to an int ID (bit representation)"""
+        border = 0
+        for i in range(len(pixels)):
+            if pixels[i] > 0:
+                border += (1 << i)
+        return border
+
+    def invertborder(self, border: int) -> int:
+        return int('{:010b}'.format(border)[::-1], 2)
+
+    def rotate(self) -> None:
+        self.borders = [
+            self.getborder(self.LEFT),
+            self.getborder(self.TOP),
+            self.getborder(self.RIGHT),
+            self.getborder(self.BOTTOM)
+        ]
+
+    def getborder(self, border: int, candidate: bool = False) -> int:
+        """
+        Return border:
+            TOP->0
+            RIGHT->1
+            BOTTOM->2
+            LEFT->3
+        """
+        assert border in range(len(self.borders))
+        if candidate:
+            return self.invertborder(
+                self.getborder(border=border, candidate=False))
+        return self.borders[border]
+
+    def flip(self, axis: int) -> None:
+        """
+        Flip tile:
+            VERTICALLY->0
+            HORIZONTALLY->1
+        """
+        assert axis in range(2)
+        if axis == self.VERTICAL:
+            self.borders = [
+                self.invertborder(self.getborder(self.BOTTOM)),
+                self.invertborder(self.getborder(self.RIGHT)),
+                self.invertborder(self.getborder(self.TOP)),
+                self.invertborder(self.getborder(self.LEFT))
+            ]
+        else:  # self.HORIZONTAL
+            self.borders = [
+                self.invertborder(self.getborder(self.TOP)),
+                self.invertborder(self.getborder(self.LEFT)),
+                self.invertborder(self.getborder(self.BOTTOM)),
+                self.invertborder(self.getborder(self.RIGHT))
+            ]
+
+    def fitsin(self, canvas, row, col):
+        checked_tiles = 0  # If no tiles around this position, then it is not declared as fitting
+        if (row - 1) >= 0:
+            if canvas[row - 1, col] != 0:
+                if canvas[row - 1, col].getborder(
+                        self.BOTTOM, candidate=True) != self.getborder(
+                            self.TOP):
+                    return False
+                else:
+                    checked_tiles += 1
+        if (row + 1) < canvas.shape[0]:
+            if canvas[row + 1, col] != 0:
+                if canvas[row + 1, col].getborder(
+                        self.TOP, candidate=True) != self.getborder(
+                            self.BOTTOM):
+                    return False
+                else:
+                    checked_tiles += 1
+        if (col - 1) >= 0:
+            if canvas[row, col - 1] != 0:
+                if canvas[row, col - 1].getborder(
+                        self.RIGHT, candidate=True) != self.getborder(
+                            self.LEFT):
+                    return False
+                else:
+                    checked_tiles += 1
+        if (col + 1) < canvas.shape[1]:
+            if canvas[row, col + 1] != 0:
+                if canvas[row, col + 1].getborder(
+                        self.LEFT, candidate=True) != self.getborder(
+                            self.RIGHT):
+                    return False
+                else:
+                    checked_tiles += 1
+        return checked_tiles > 0  # Prevents it to be placed in an empty place
+
+    # For pretty-printing the canvas
+    def __repr__(self):
+        return f"T{self.id}"
