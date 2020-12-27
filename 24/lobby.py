de42d0f4418d8be37ab8dad580f64b37e3dae1d8
guillemglez <guillemglez@gmail.com>
2020-12-27 21:27:22 +0100

    Day 24, part 2

--- 24/lobby.py
+++ 24/lobby.py
@@ -1,6 +1,10 @@
 from typing import List, Optional
 import numpy as np
 
+BLACK = True
+WHITE = False
+DAYS = 100
+
 
 def vector(cardinal: str) -> np.array:
     if cardinal == 'e':
@@ -35,7 +39,7 @@ with open('input') as f:
         tiles.append(direction)
 
 radius = max([np.hypot(*direction) for direction in tiles])
-lobby = np.zeros((int(radius * 2 + 1), ) * 2, dtype=bool)
+lobby = np.zeros((int(radius * 2 + DAYS * 2 + 1), ) * 2, dtype=bool)
 central = tuple(s // 2 for s in lobby.shape)
 
 for tile in tiles:
@@ -43,4 +47,35 @@ for tile in tiles:
     crow, ccol = central
     lobby[row + crow, col + ccol] = not lobby[row + crow, col + ccol]
 
-print(f"There are {np.count_nonzero(lobby)} tiles left with the black side up")
+print(
+    f"There are {np.count_nonzero(lobby)} tiles left with the black side up after the instructions have been followed"
+)
+
+for day in range(DAYS):
+    yesterday = lobby.copy()
+    for idx, tile in np.ndenumerate(yesterday):
+        row, col = idx
+        black = 0
+        if (row - 1) >= 0:
+            if (col - 2) >= 0:
+                black += yesterday[row, col - 2]
+                black += yesterday[row - 1, col - 1]
+            if (col + 2) < lobby.shape[1]:
+                black += yesterday[row, col + 2]
+                black += yesterday[row - 1, col + 1]
+        if (row + 1) < lobby.shape[0]:
+            if (col - 2) >= 0:
+                black += yesterday[row + 1, col - 1]
+            if (col + 2) < lobby.shape[1]:
+                black += yesterday[row + 1, col + 1]
+
+        if tile == BLACK:
+            if black == 0 or black > 2:
+                lobby[idx] = WHITE
+        if tile == WHITE:
+            if black == 2:
+                lobby[idx] = BLACK
+
+print(
+    f"There are {np.count_nonzero(lobby)} tiles left with the black side up after {DAYS} days"
+)
